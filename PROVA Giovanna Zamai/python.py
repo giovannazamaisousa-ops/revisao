b32b5a8f3872a23ae5677b996a2e41dc6e36d085
giovannazs <giovanna.zamai.sousa@escola.pr.gov.br>
2026-08-13 10:17:35 -0300

    Add files via upload

---
+++ PROVA Giovanna Zamai/python.py
@@ -0,0 +1,8 @@
+lista = ["Banana", "Mamão", "Melão", "kiwi", "Goiaba"]
+
+
+lista.insert(2, "Maracuja")
+lista.append("Bergamota")
+lista.remove("Banana")
+lista.pop()
+print(lista)
---
+++ PROVA Giovanna Zamai/script.js
@@ -0,0 +1,6 @@
+let nome = "Giovanna"
+let idade = 17
+let altura = 1.52
+let cidade = "Maringá"
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos, ${altura} de altura e moro em ${cidade}.`);
